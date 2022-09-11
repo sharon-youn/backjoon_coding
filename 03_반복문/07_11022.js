c05e0f359010fafd73dd92d0d2949e237b228f8c
sharon-youn <jos9187@naver.com>
2022-09-11 17:36:51 +0900

    9.11 문제 풀기

---
+++ 03_반복문/07_11022.js
@@ -0,0 +1,21 @@
+// run.js
+
+const fs = require("fs");
+ // node.js의 fs 모듈을 사용해 파일을 읽어온다
+// 출력이 다음 줄에 있을땐 split('\n')으로 하고 
+// 옆에 있을떈 split(' ')로 하기!! 
+const input= require('fs').readFileSync('dev/stdin').toString().split('\n')
+//require('fs').readFileSync('dev/stdin')
+//예제.txt
+// 꼭 바꿔주기
+
+// 테스트케이스 개수 total
+let total = parseInt(input[0])
+
+
+
+// 테스트케이스 0번째 값 제외 1부터 시작! 
+for(let i =1; i<= total; i++) {
+  let [A, B] = input[i].split(' ').map(Number)
+  console.log(`Case #${i}: ${A} + ${B} = ${A+B}`);
+}
---
+++ 03_반복문/08_2438.js
@@ -0,0 +1,20 @@
+// run.js
+
+const fs = require("fs");
+ // node.js의 fs 모듈을 사용해 파일을 읽어온다
+// 출력이 다음 줄에 있을땐 split('\n')으로 하고 
+// 옆에 있을떈 split(' ')로 하기!! 
+const input= require('fs').readFileSync('dev/stdin').toString()
+//require('fs').readFileSync('dev/stdin')
+//예제.txt
+// 꼭 바꿔주기
+
+
+// 5가 입력됐을때 *이 1,2,3,4,5 가 찍혀야한다.
+let result = ''; 
+let star = '*'
+// 테스트케이스 0번째 값 제외 1부터 시작! 
+for(let i =1; i<= input; i++) {
+  result = result.concat(star)
+  console.log(result)
+}
