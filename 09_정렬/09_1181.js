3e00e3241fe248d880b8e78a60836469400dd4e5
sharon-youn <jos9187@gmail.com>
2022-10-04 01:44:06 +0900

    정렬 9번 완료

---
+++ 09_정렬/09_1181.js
@@ -0,0 +1,36 @@
+const fs = require("fs");
+// 옆에 있을떈 split(' ')로 하기!! 
+const input= fs.readFileSync('dev/stdin').toString().trim().split('\n')
+//require('fs').readFileSync('dev/stdin')
+//예제.txt
+
+// 단어의 개수 num만 일단 빼기 
+let num = input.shift()
+
+//길이가 짧은 것 부터
+
+// 단어 길이 관련 객체 만들어주기 
+let obj = {};
+for(let i =0 ; i<num ; i++) {
+        obj[input[i]] = input[i].length;    
+}
+// console.log(obj)
+
+// 객체를 2차원 배열로 바꾸기 
+// [ ['but', 3], [...]]
+let arr = [];
+for(let el in obj) {
+    arr.push([el, obj[el]])
+}
+
+// 소문자 정렬 먼저 해주기
+arr = arr.sort()
+// 단어 길이 정렬하기 
+arr.sort((a,b) => {
+    return a[1] - b[1] 
+})
+let result = ''
+for(let el of arr) {
+    result += `${el[0]}\n`
+}
+console.log(result)
